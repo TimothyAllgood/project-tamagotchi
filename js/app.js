1e1d9a1559bf42fdc816cc10fa26e289c73c94dd
Timothy Allgood <timothyallgood@gmail.com>
2020-06-22 12:32:48 -0500

    Added variables for DOM manipulation

--- js/app.js
+++ js/app.js
@@ -15,3 +15,20 @@ class Pet{
 const pet = new Pet;
 
 console.log(pet);
+
+//------------DOM Elements
+//Hunger Elements
+const hungerText = document.querySelector('#hunger p');
+const hungerBar = document.querySelector('#hunger .status-color');
+const hungerBtn = document.querySelector('#hunger button');
+
+//Sleep Elements
+const sleepText = document.querySelector('#sleepiness p');
+const sleepBar = document.querySelector('#sleepiness .status-color');
+const sleepBtn = document.querySelector('#sleepiness button');
+
+//Boredom Elements
+
+const boredText = document.querySelector('#boredom p');
+const boredBar = document.querySelector('#boredom .status-color');
+const boredBtn = document.querySelector('#boredom button');
