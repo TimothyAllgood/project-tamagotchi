95eee2a206dc4b6db2eef80f0bdcba816fa0b38f
Timothy Allgood <timothyallgood@gmail.com>
2020-06-22 12:26:29 -0500

    Created class for pet, and instatiated pet

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 console.log('Welcome to Project Tamagotchi!')
+
+// Create Class For Pet
+class Pet{
+    constructor(){
+        this.name = 'Pet';
+        this.hunger = 1;
+        this.sleep = 1;
+        this.boredom = 1;
+        this.time = 10;
+    }
+}
+
+//Instantiate Pet
+const pet = new Pet;
+
+console.log(pet);
