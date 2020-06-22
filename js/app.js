96b75e6e84bcb4eb71625901728a5d5d4de83928
Timothy Allgood <timothyallgood@gmail.com>
2020-06-22 13:42:51 -0500

    Added header, styled header, updated functions to update timer

--- js/app.js
+++ js/app.js
@@ -10,7 +10,8 @@ class Pet{
         this.sleepWidth = 0;
         this.boredom = 0;
         this.boredomWidth = 0;
-        this.time = 10;
+        this.time = 60;
+        this.age = 1;
     }
 }
 
@@ -36,16 +37,25 @@ const boredText = document.querySelector('#boredom p');
 const boredBar = document.querySelector('#boredom .status-color');
 const boredBtn = document.querySelector('#boredom button');
 
+//Timer Elements
+const timerText = document.querySelector('header p');
+
 timerControl();
 
 function timerControl(){
     const timer = setInterval(()=>{
         if(pet.time>0){
             pet.time--;
-            // console.log(pet.time)
+            updateTime();
             raiseHunger();
             raiseSleep();
             raiseBoredom();
+            // Every x seconds increase pet age
+            if(pet.time % 30 === 0){
+                pet.age++;
+                console.log('Age:', pet.age);
+            }
+            // If pet hunger, sleep, or boredom stat reaches 10, player loses, and game ends
             if(pet.hunger === 10 || pet.sleep === 10 || pet.boredom === 10){
                 endGame(timer);
             }
@@ -82,9 +92,8 @@ function raiseHunger(){
     hungerText.textContent = `${pet.hunger}/10`
     pet.hungerWidth += 10;
     hungerBar.style.width = `${pet.hungerWidth}%`
-    console.log('Hunger: ', pet.hungerWidth);
+    console.log('Hunger: ', pet.hunger);
 }
-
 
 // Sleep Functions
 function turnOffLights(){
@@ -105,13 +114,12 @@ function raiseSleep(){
     console.log('Sleep: ', pet.sleep);
 }
 
-
 // Boredom Functions
 function playWithPet(){
     if(pet.boredom > 1){
         pet.boredom--;
         boredText.textContent = `${pet.boredom}/10`
-        pet.boredWidth -= 10;
+        pet.boredomWidth -= 10;
         boredBar.style.width = `${pet.boredomWidth}%`
         console.log(pet.boredom);
     }
@@ -124,11 +132,29 @@ function raiseBoredom(){
     boredBar.style.width = `${pet.boredomWidth}%`
     console.log('Boredom: ', pet.boredom);
 }
+// UI Functions
+// changes time to MM:SS format
+function prettyTime(string,pad,length) {
+    return (new Array(length+1).join(pad)+string).slice(-length);
+}
 
+function convertTime(){
+    let minutes = Math.floor(pet.time / 60);
+    let seconds = pet.time -  minutes * 60;
+    return prettyTime(minutes,'0',2)+':' + prettyTime(seconds,'0',2);
+    
+}
+// updates timer text
+function updateTime(){
+    timerText.textContent = `${convertTime()}`;
+}
 
 // End game
 
 function endGame(timer){
-    if(pet.hunger || pet.sleep || pet.boredom)
+    //Remove listeners once game is over, fixes bug that allows player to adjust stat values after game is over
+    hungerBtn.removeEventListener('click', feedPet);
+    sleepBtn.removeEventListener('click', turnOffLights);
+    boredBtn.removeEventListener('click', playWithPet);
     clearInterval(timer);
 }
